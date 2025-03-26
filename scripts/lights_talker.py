9645cb569a9449e5a6f8f527b7eb1c2f6b0acf25
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-26 12:46:46 -0700

    more debugging

--- scripts/lights_talker.py
+++ scripts/lights_talker.py
@@ -31,6 +31,7 @@ def talker():
 		switchState(torsoRightLightOuter)
 	for i in range(0, 10):
 		switchLights()
+		leftLightInner = not leftLightInner
 		print("Switched the lights!", i)
 		rospy.sleep(1)
 
