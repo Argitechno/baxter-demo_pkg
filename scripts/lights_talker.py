2d289f0a4222e00fd00b39800313e5f0397d27a6
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-26 12:47:53 -0700

    debugging never ends

--- scripts/lights_talker.py
+++ scripts/lights_talker.py
@@ -11,27 +11,27 @@ import baxter_interface.digital_io as DIO
 def talker():
 	rospy.init_node('lights_talker')
 	leftLightInner = DIO.DigitalIO('left_inner_light')
-	leftLightOuter = DIO.DigitalIO('left_outer_light')
-	rightLightInner = DIO.DigitalIO('right_inner_light')
-	righLightOuter = DIO.DigitalIO('right_outer_light')
-	torsoLeftLightInner = DIO.DigitalIO('torso_left_inner_light')
-	torsoLeftLightOuter = DIO.DigitalIO('torso_left_outer_light')
-	torsoRightLightInner = DIO.DigitalIO('torso_right_inner_light')
-	torsoRightLightOuter = DIO.DigitalIO('torso_right_outer_light')
+	#leftLightOuter = DIO.DigitalIO('left_outer_light')
+	#rightLightInner = DIO.DigitalIO('right_inner_light')
+	#righLightOuter = DIO.DigitalIO('right_outer_light')
+	#torsoLeftLightInner = DIO.DigitalIO('torso_left_inner_light')
+	#torsoLeftLightOuter = DIO.DigitalIO('torso_left_outer_light')
+	#torsoRightLightInner = DIO.DigitalIO('torso_right_inner_light')
+	#torsoRightLightOuter = DIO.DigitalIO('torso_right_outer_light')
 	def switchLights():
 		def switchState(digitalComponent):
 			digitalComponent = not digitalComponent
 		switchState(leftLightInner)
-		switchState(leftLightOuter)
-		switchState(rightLightInner)
-		switchState(righLightOuter)
-		switchState(torsoLeftLightInner)
-		switchState(torsoLeftLightOuter)
-		switchState(torsoRightLightInner)
-		switchState(torsoRightLightOuter)
+		#switchState(leftLightOuter)
+		#switchState(rightLightInner)
+		#switchState(righLightOuter)
+		#switchState(torsoLeftLightInner)
+		#switchState(torsoLeftLightOuter)
+		#switchState(torsoRightLightInner)
+		#switchState(torsoRightLightOuter)
 	for i in range(0, 10):
 		switchLights()
-		leftLightInner = not leftLightInner
+		#leftLightInner = not leftLightInner
 		print("Switched the lights!", i)
 		rospy.sleep(1)
 
