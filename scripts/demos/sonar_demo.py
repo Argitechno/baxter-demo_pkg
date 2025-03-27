7253762224e9e7c0b58822f0e29c203080ce72ab
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-27 13:46:30 -0700

    Add setting lights to sonar_demo

--- scripts/demos/sonar_demo.py
+++ scripts/demos/sonar_demo.py
@@ -8,11 +8,18 @@ import demo_pkg.sonar_io as SIO
 def main():
     rospy.init_node('sonar_demo', anonymous=True)
     s = SIO.SonarIO()
-    
+    l = s.lights()
     for i in range(0, 12):
         s.set_sonars(2**i)
         print("State :", len(s.state()), "Enabled: ", bin(s.get_sonars()))
-        rospy.sleep(5)
+        rospy.sleep(1)
+    
+    rate = rospy.Rate(100)
+    targetExit = rospy.Time.now() + rospy.Duration(5)
+    while rospy.Time.now() < targetExit:
+        l.set_lights(int('0b101010101010',2))
+        rate.sleep()
+        
 
 if __name__ == '__main__':
     main()
