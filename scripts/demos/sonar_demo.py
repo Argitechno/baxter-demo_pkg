fc84900b3879e2e94285b54069746b35b21b711d
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-27 13:02:00 -0700

    EE

--- scripts/demos/sonar_demo.py
+++ scripts/demos/sonar_demo.py
@@ -9,10 +9,9 @@ def main():
     rospy.init_node('sonar_demo', anonymous=True)
     s = SIO.SonarIO()
     
-    for i in range(1, 2**15):
-        s.set_sonars(i)
+    for i in range(0, 12):
+        s.set_sonars(2**i)
         print("State :", len(s.state()), "Enabled: ", bin(s.get_sonars()))
-        i = i * 2
         rospy.sleep(5)
 
 if __name__ == '__main__':
