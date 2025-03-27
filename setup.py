91a7bd170bc1394189c7c7a2c0b6444f4cba0f8c
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-27 11:52:18 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup()
-setup_args['packages'] = ['sonar_io']
+setup_args['packages'] = ['demo_pkg']
 setup_args['package_dir'] = {'': 'src'}
 
 setup(**setup_args)
