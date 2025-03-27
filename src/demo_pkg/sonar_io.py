d4ac509fecc7b95a73713b202ee13f193934442b
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-27 13:35:35 -0700

    Added light control.

--- src/demo_pkg/sonar_io.py
+++ src/demo_pkg/sonar_io.py
@@ -15,8 +15,10 @@ class SonarIO(object):
     #We need to have a subscriber for all the information topics for sonar.
     #And we need to have a publisher for all the lights for sonar.
     #And then we can make public functions to access or set these.
+
     def __init__(self):
         topic_base = "/robot/sonar/head_sonar"
+        self.lights = self._Lights()
         self._sonars = 0
         #State is a dictionary for the elements of PointCloud
         self._state = dict()
@@ -78,7 +80,7 @@ class SonarIO(object):
     def set_sonars(self, value, timeout=2.0):
         #Control which sonars are enabled.
         #@type value: uint16
-        #@param value: Int rep of binary for which sonars to have on. (Ex: 0b000000101010, 6th, 4th, and 2nd sonars on.)
+        #@param value: Int rep of binary for which sonars to have on. (Ex: 0b000000101010, 6th, 4th, and 2nd sonars on.) First sonar is facing foward, they go around clockwise.
         #@type timeout: float
         #@param timeout: Seconds to wait for the io to reflect command.
         #                If 0, just command once and return. [0]
@@ -94,11 +96,68 @@ class SonarIO(object):
                 timeout_msg=("Failed to command sonars to: %d" % (value,)),
                 body=lambda: self._pub_sonars.publish(cmd)
             )
-
-        pass
         
-
     def get_sonars(self):
         #Get the value of representing if the sonars are on or not.
         #Int rep of binary for which sonars are on. (Ex: 0b000000101010, 6th, 4th, and 2nd sonars on.)
         return self._sonars
+    
+    class _Lights(object):
+        def __init__(self):
+            topic_base = "/robot/sonar/head_sonar/lights"
+            self._lights = 0
+            #State is... just an UInt16. but we wanna make sure its their, so we will see if the 'data' value exists later.
+            self._state = dict()
+
+            #Make a subscriber for the state
+            self._sub_state = rospy.Subscriber(
+                topic_base + '/state',
+                UInt16,
+                self._on_lights_state)
+
+            #Make sure that we HAVE the state
+            baxter_dataflow.wait_for(
+                lambda: len(self._state.keys()) != 0,
+                timeout=2.0,
+                timeout_msg="Failed to get current sonar state from %s" \
+                % (topic_base,),
+            )
+
+            #Make a publisher for set_lights
+            self._pub_lights = rospy.Publisher(
+                    topic_base + '/set_lights',
+                    UInt16,
+                    queue_size=10)
+
+        def _on_lights_state(self, msg):
+            #Somewhat redundant...
+            self._state['data'] = msg.data
+            self._lights = msg.data
+
+        def set_lights(self, value, timeout=2.0):
+            #Control which sonar lights are enabled.
+            #This must be done at a fast rate or it doesn't work, from documentation i can find that should be 100+
+            #@type value: uint16
+            #@param value: Int rep of binary for which sonars to have on. (Ex: 0b000000101010, 6th, 4th, and 2nd sonars on.) First sonar is facing foward, they go around clockwise.
+            #@type timeout: float
+            #@param timeout: Seconds to wait for the io to reflect command.
+            #                If 0, just command once and return. [0]
+            cmd = value
+            #cmd.data = value
+            print(cmd)
+            self._pub_lights.publish(cmd)
+            if not timeout == 0:
+                baxter_dataflow.wait_for(
+                    test=lambda: self._lights == value,
+                    timeout=timeout,
+                    rate=100,
+                    timeout_msg=("Failed to command sonars to: %d" % (value,)),
+                    body=lambda: self._pub_lights.publish(cmd)
+                )   
+
+        def get_lights(self):
+        #Get the value of representing if the sonars are on or not.
+        #Int rep of binary for which sonars are on. (Ex: 0b000000101010, 6th, 4th, and 2nd sonars on.)
+            return self._lights
+    
+        pass
