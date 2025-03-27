159f800e49730a4c8f89ec8beedf22e40d257816
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-27 11:26:35 -0700

    Gave sonar_demo a main

--- scripts/demos/sonar_demo.py
+++ scripts/demos/sonar_demo.py
@@ -2,11 +2,15 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
-import sonar_io as SIO
+import demo_pkg.sonar_io as SIO
 
-rospy.init_node('sonar_demo', anonymous=True)
 
-s = SIO.SonarIO
-for i in range(0, 100):
-    print(len(s.state()))
-    rospy.sleep(0.2)
+def main():
+    rospy.init_node('sonar_demo', anonymous=True)
+    s = SIO.SonarIO
+    for i in range(0, 100):
+        print(len(s.state()))
+        rospy.sleep(0.2)
+
+if __name__ == '__main__':
+    main()
