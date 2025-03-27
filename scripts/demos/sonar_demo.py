1f2b342ca20e2841374e8bbc7d2ef7d0e113697f
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-27 12:31:44 -0700

    Added commanding sonars to sonar_demo

--- scripts/demos/sonar_demo.py
+++ scripts/demos/sonar_demo.py
@@ -8,8 +8,9 @@ import demo_pkg.sonar_io as SIO
 def main():
     rospy.init_node('sonar_demo', anonymous=True)
     s = SIO.SonarIO()
+    s.set_sonars(int('0b1000011',2))
     for i in range(0, 100):
-        print("State :", len(s.state()), "Enabled: ", bin(s.get_sonars_enabled()))
+        print("State :", len(s.state()), "Enabled: ", bin(s.get_sonars()))
 
         rospy.sleep(0.2)
 
