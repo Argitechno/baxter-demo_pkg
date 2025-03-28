021ea865aa23c55f8eb040239d733e10d69a9993
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-28 12:34:32 -0700

    fixed logic error

--- scripts/demos/gripper_demo.py
+++ scripts/demos/gripper_demo.py
@@ -4,15 +4,16 @@ import rospy
 import baxter_interface
 
 def main():
-        gripper = baxter_interface.Gripper('right')
-        print("Gripper Demo!")
-        gripper.calibrate()
-        # Move to positions 0, 100, 75, 50, 25, 0
-        for i in range(5, -1, -1):
-            gripper.command_position((i % 5) * 25)
-            rospy.sleep(3)
+    rospy.init_node('test_gripper')
+    gripper = baxter_interface.Gripper('right')
+    print("Gripper Demo!")
+    gripper.calibrate()
+    # Move to positions 0, 100, 75, 50, 25, 0
+    for i in range(5, -1, -1):
+        gripper.command_position((i % 5) * 25)
+        rospy.sleep(3)
 
 if __name__ == '__main__':
-    rospy.init_node('test_gripper')
+    main()
 
 
