c53211a638f315065368a3eba92eb2bf65a765b9
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-28 12:33:41 -0700

    Made gripper_demo briefer.

--- scripts/demos/gripper_demo.py
+++ scripts/demos/gripper_demo.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python 
-#   
-"""
-Script to test gripper
-"""
-
-# rospy - ROS Python API
+#!/usr/bin/env python
+# license removed for brevity
 import rospy
-
-# baxter_interface - Baxter Python API
 import baxter_interface
 
-# initialize our ROS node, registering it with the Master
-rospy.init_node('test_gripper')
+def main():
+        gripper = baxter_interface.Gripper('right')
+        print("Gripper Demo!")
+        gripper.calibrate()
+        # Move to positions 0, 100, 75, 50, 25, 0
+        for i in range(5, -1, -1):
+            gripper.command_position((i % 5) * 25)
+            rospy.sleep(3)
 
-# create instance of baxter_interface's Limb class
-gripper = baxter_interface.Gripper('right')
-print("Gripper Demo!")
-gripper.close()
-gripper.open()
-rospy.sleep(3)
-gripper.command_position(75)
-rospy.sleep(3)
-gripper.command_position(50)
-rospy.sleep(3)
-gripper.command_position(0)
-rospy.sleep(3)
-gripper.command_position(100)
-quit()
+if __name__ == '__main__':
+    rospy.init_node('test_gripper')
+
+
