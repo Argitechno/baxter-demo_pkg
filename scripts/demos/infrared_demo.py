836aecef14ed7807e9eda3c418c98f005430d3f0
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-28 12:56:39 -0700

    Added infrared demo

---
+++ scripts/demos/infrared_demo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import baxter_interface.analog_io as AIO
+
+
+def main():
+    print("Initializing node...")
+    rospy.init_node('infrared_demo', anonymous=True)
+    
+    irs = AIO.AnalogIO('right_hand_range')
+    rate = rospy.Rate(2)
+    targetExit = rospy.Time.now() + rospy.Duration(5)
+
+    print("Starting range monitor...")
+    while rospy.Time.now() < targetExit:
+        print(irs.state())
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
