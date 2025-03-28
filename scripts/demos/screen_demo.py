ddf2faf16d207c7ff1cbcc763a8f45df0c145027
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-28 14:32:09 -0700

    Added some checking to screen_demo

--- scripts/demos/screen_demo.py
+++ scripts/demos/screen_demo.py
@@ -3,19 +3,28 @@
 import rospy
 import cv2
 import cv_bridge
+from pathlib import Path
+
+
 from sensor_msgs.msg import Image
 
 def main():
+    rickroll = Path('../../assets/rickroll.mp4')
+    if not rickroll.exists():
+        print("No file exists at: ")
+        print(rickroll)
+        return
     rospy.init_node('screen_demo', anonymous=True)
     pub = rospy.Publisher('/robot/xdisplay', Image, latch = True, queue_size = 1)
-    video = cv2.VideoCapture('../../assets/rickroll.mp4')
+    video = cv2.VideoCapture(rickroll)
 
-    rate = rospy.Rate(60)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    rate = rospy.Rate(fps)
     while True:
         _, frame = video.read()
         print(frame)
         msg = cv_bridge.CvBridge().cv2_to_imgmsg(frame, encoding="bgr8")
-
+        pub.publish(msg)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break   
         rate.sleep()
