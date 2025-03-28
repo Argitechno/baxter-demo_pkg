71a389e4e68ce7a18cb87b148edc35fa068a7c2b
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-28 11:51:52 -0700

    Update to check the cameras.

--- scripts/demos/camera_demo.py
+++ scripts/demos/camera_demo.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-import os
+import rosgraph
+import socket
 import cv2
 import cv_bridge
 from sensor_msgs.msg import Image
+from baxter_core_msgs.srv import ListCameras
 from baxter_interface.camera import CameraController
 
 def open_cam(camera, res):
@@ -13,17 +15,41 @@ def open_cam(camera, res):
     cam = CameraController(camera) 
     cam.resolution = res 
     cam.open()
+
 def close_cam(camera):
     cam = CameraController(camera)
     cam.close()
+
+def list_cameras():
+    ls = rospy.ServiceProxy('cameras/list', ListCameras)
+    rospy.wait_for_service('cameras/list', timeout = 10)
+    resp = ls()
+    if len(resp.cameras):
+        master = rosgraph.Master('/rostopic')
+        resp.cameras
+        cam_topics = dict([(cam, "/cameras/%s/image" % cam) for cam in resp.cameras])
+        open_cams = dict([(cam, False) for cam in resp.cameras])
+        try:
+            topics = master.getPublishedTopics('')
+            for topic in topics:
+                for cam in resp.cameras:
+                    if topic[0] == cam_topics[cam]:
+                            open_cams[cam] = True
+        except socket.error:
+            raise ROSTopicIOException("Cannot communicate with master.")
+    return open_cams
+                
+
 
 def main():
     print("Initializing Node")
     rospy.init_node('camera_demo', anonymous=True)
 
-    print("Closing left hand camera")
-    close_cam('left_hand_camera')
+    cameras = list_cameras()
 
+    if(not cameras['head_camera'] and cameras['left_hand_camera']):
+        print("Closing left hand camera")
+        close_cam('left_hand_camera')
     print("Opening head camera")
     open_cam('head_camera', (1280, 700))
 
