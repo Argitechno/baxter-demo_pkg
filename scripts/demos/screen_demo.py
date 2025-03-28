4dd1bf37a5fa1e4260559e176b8ee6ad2103b55c
Caleb Blondell <caleb.blondell@gmail.com>
2025-03-28 14:35:26 -0700

    no pathlib

--- scripts/demos/screen_demo.py
+++ scripts/demos/screen_demo.py
@@ -3,20 +3,20 @@
 import rospy
 import cv2
 import cv_bridge
-from pathlib2 import Path
+import os.path
 
 
 from sensor_msgs.msg import Image
 
 def main():
-    rickroll = Path('../../assets/rickroll.mp4')
-    if not rickroll.exists():
+    rickroll = '../../assets/rickroll.mp4'
+    if not os.path.isfile(rickroll):
         print("No file exists at: ")
         print(rickroll)
         return
     rospy.init_node('screen_demo', anonymous=True)
     pub = rospy.Publisher('/robot/xdisplay', Image, latch = True, queue_size = 1)
-    video = cv2.VideoCapture(rickroll)
+    video = cv2.VideoCapture(os.path(rickroll))
 
     fps = video.get(cv2.CAP_PROP_FPS)
     rate = rospy.Rate(fps)
