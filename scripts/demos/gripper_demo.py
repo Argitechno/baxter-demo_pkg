6b243d78df366cd534291401e6403537d8e63b9f
Caleb Blondell <caleb.blondell@gmail.com>
2025-04-04 15:18:51 -0700

    gripper_demo doc/safe

--- scripts/demos/gripper_demo.py
+++ scripts/demos/gripper_demo.py
@@ -4,9 +4,9 @@ import baxter_interface.cfg
 import rospy
 import baxter_interface
 
-def main():
-    print("Initializing node...")
-    rospy.init_node('gripper_demo')
+
+def gripper_demo():
+    """Access the gripper and move it, and tell us the pose of the endpoint (where the gripper is)"""
     gripper = baxter_interface.Gripper('right')
     right_limb = baxter_interface.Limb('right')
     print("Gripper Demo!")
@@ -23,6 +23,25 @@ def main():
         gripper.command_position(i  * 25)
         rospy.sleep(3)
     gripper.open()
+
+def main():
+    """Demonstrate usage of the gripper."""
+
+    print("Initializing node...")
+    rospy.init_node('gripper_demo')
+    print("Getting robot state...")
+    rs = baxter_interface.RobotEnable(CHECK_VERSION)
+    init_state = rs.state().enabled
+    
+    def clean_shutdown():
+        print("\nExiting example...")
+        if not init_state:
+            print("Disabling robot...")
+            rs.disable()
+    
+    rospy.on_shutdown(clean_shutdown)
+    gripper_demo()
+    
 
 if __name__ == '__main__':
     main()
